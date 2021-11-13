674e4334eda2481105b65963898599b2f3d14e18
brian-fernando <kavinda98fernando@gmail.com>
2021-11-13 14:14:14 +0530

    Created helloNext class

---
+++ DemoHello/src/nxtMyCode/HelloNext.java
@@ -0,0 +1,14 @@
+package nxtMyCode;
+
+public class HelloNext {
+
+	public HelloNext() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
